642d753561a7b7a6a8163d146d50e67ad1e7a87a
刘旺旺 <hpuliuwangwang@163.com>
2018-10-11 10:54:21 +0800

    eureka服务提供者

---
+++ eureka-client/src/main/java/com/liuww/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.liuww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * eureka 服务提供者主类
+ * 使用 @EnableDiscoveryClient 注解将当前应用加入到服务治理体系中
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
