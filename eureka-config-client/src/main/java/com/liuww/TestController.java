816a6be987bb6357c94194d1b258b5b4df893ac5
刘旺旺 <hpuliuwangwang@163.com>
2018-10-24 18:25:22 +0800

    动态刷新配置

--- eureka-config-client/src/main/java/com/liuww/TestController.java
+++ eureka-config-client/src/main/java/com/liuww/TestController.java
@@ -1,15 +1,18 @@
 package com.liuww;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 测试
+ * @RefreshScope 注解，用于配置文件更新时处理
  *
  * @author liuww
  * @date 2018/10/22 16:01
  */
+@RefreshScope
 @RestController
 public class TestController {
 
