2eadea8863a1eae05f7359b8edec69602b66a433
刘旺旺 <hpuliuwangwang@163.com>
2018-10-24 17:32:53 +0800

    配置中心连接到eureka注册中心

---
+++ eureka-config-client/src/main/java/com/liuww/EurekaConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.liuww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigClientApplication.class, args);
+    }
+}
