feeecf35f5f751cea40081b47a935356787f351c
刘旺旺 <hpuliuwangwang@163.com>
2018-10-25 15:29:10 +0800

    Hystrix监控面板

---
+++ hystrix-dashboard/src/main/java/com/liuww/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.liuww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * 加上@EnableHystrixDashboard，启用Hystrix Dashboard功能
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
