0df7ce6e8667934b30ac1c1e9606c232de408e51
刘旺旺 <hpuliuwangwang@163.com>
2018-10-26 14:09:56 +0800

    使用Turbine来对服务的Hystrix数据进行聚合展示

---
+++ turbine/src/main/java/com/liuww/TurbineApplication.java
@@ -0,0 +1,17 @@
+package com.liuww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 使用@EnableTurbine注解开启Turbine
+ */
+@EnableTurbine
+@SpringBootApplication
+public class TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineApplication.class, args);
+    }
+}
