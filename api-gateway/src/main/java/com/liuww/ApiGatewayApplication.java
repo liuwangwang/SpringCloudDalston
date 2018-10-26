77a48256127fdc6d583aaa6e4ac82ae20a778880
刘旺旺 <hpuliuwangwang@163.com>
2018-10-26 14:30:50 +0800

    使用Spring Cloud Zuul构建服务网关

---
+++ api-gateway/src/main/java/com/liuww/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.liuww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 使用@EnableZuulProxy注解开启Zuul的功能
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
